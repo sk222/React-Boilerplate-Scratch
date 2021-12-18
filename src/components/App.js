033e3131a58f1a12ac47490368bb5d67785f6d21
sk222 <skhanna428@gmail.com>
2021-12-17 21:37:16 -0700

    :tada:

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<h1>
+					Application Boilerplate Without <br/>Create-React-App <br/>Installed Webpack,
+					Babel seprately
+				</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
